e482cd9f7724e6a0d546f2e1690ebf8362c1ee9b
Evgeniy Zemtsov <zemtsov.e.e@gmail.com>
2016-03-12 23:26:21 +0300

    Moving to recursive structure

--- homeworks/music_library/lib/Local/TableRenderer.pm
+++ homeworks/music_library/lib/Local/TableRenderer.pm
@@ -12,27 +12,100 @@ my $nrows;
 my $ncols;
 my @widths;
 
-sub header {
-    my ($w) = @_;
+my %shells = (
+    header => {
+        left        => '/-',
+        right       => "-\\\n",
+        separator   => '---',
+        placeholder => '-',
+    },
 
-    print '/' , '-'x($w-2) , "\\\n" ;
-}
+    footer => {
+        left        => '\\-',
+        right       => "-/\n",
+        separator   => '---',
+        placeholder => '-',
+    },
 
-sub h_separator {
-    my ($w) = @_;
+    row_separator => {
+        left        => '|-',
+        right       => "-|\n",
+        separator   => '-+-',
+        placeholder => '-',
+    },
 
-    print '/' , '-'x($w-2) , "\\\n" ;
-}
+    data_row => {
+        left        => '| ',
+        right       => " |\n",
+        separator   => ' | ',
+    },
 
-sub footer {
-    my ($w) = @_;
+    cell => {
+        left        => sub {},
+        right       => sub {},
+    },
+);
 
-    print '\\' , '-'x($w-2) , "/\n" ;
-}
+$shells{table} = {
+    left        => build_line($shells{header}),
+    right       => build_line($shells{footer}),
+    separator   => build_line($shells{row_separator}),
+    subshell    => $shells{data_row},
+};
 
 sub pad {
-    my ($str, $w) = @_;
-    return ' 'x($w-length($str)) . $str;
+    my ($row) = @_;
+
+    my @padded_row
+        = map { ' 'x($widths[$_]-length($row->[$_])) . $row->[$_] } 0..$ncols-1;
+
+    return @padded_row;
+}
+
+sub separate {
+   my ($row, $sep) = @_;
+
+   my @separated_row = map { ($_, $sep) } @$row;
+   pop @separated_row;
+
+   return \@separated_row;
+}
+
+sub wrap {
+    my ($row, $left, $right) = @_;
+
+    my @wrapped_row = ($left, @$row, $right);
+
+    return \@wrapped_row;
+}
+
+sub build_block {
+    my $shell = shift;
+    my @data;
+
+    if (defined $shell->{placeholder}) {
+        @data = map { $shell->{placeholder}x$widths[$_] } 0..$ncols-1; 
+    }
+    else {
+        @data = pad(shift);
+    }
+
+	local $" = $shell->{separator};
+	local $, = '';
+
+    return $shell->{left} . "@data" . $shell->{right};
+}
+
+sub build_line {
+    my $shell = shift;
+    my $data  = shift;
+    my $line;
+
+    local *STDOUT;
+    open STDOUT, '>', \$line;
+    build_block($shell, $data);
+
+    return $line;
 }
 
 sub render {
@@ -44,11 +117,21 @@ sub render {
         $widths[$i] = max( map { length $_->[$i] } @$matrix );
     }
 
-    for my $row (@$matrix) {
-        print '|'; 
-        map { print ' ' . pad($row->[$_], $widths[$_]) . ' |' } 0..$ncols-1;
-        print "\n";
-    }
+    my $row_separator = build_line($shells{row_separator});
+
+    #    build_block($shells{table});
+    #
+    #    {
+    #        local $" = $row_separator;
+    #        local $, = '';
+    #
+    #        my @rows = map { build_line($shells{data_row}, $_) } @$matrix;
+    #
+    #        print "@rows";
+    #    }
+    #
+    #    build_block($shells{footer});
+
 
     print join ' ', @widths , "\n";
    
