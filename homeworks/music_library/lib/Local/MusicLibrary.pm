ba223e3a200b17c54957e9d5219c9e54f24e5931
Evgeniy Zemtsov <zemtsov.e.e@gmail.com>
2016-03-12 16:47:57 +0300

    Add initial parser

--- homeworks/music_library/lib/Local/MusicLibrary.pm
+++ homeworks/music_library/lib/Local/MusicLibrary.pm
@@ -2,6 +2,10 @@ package Local::MusicLibrary;
 
 use strict;
 use warnings;
+use Data::Dumper;
+use Exporter 'import';
+
+our @EXPORT_OK = qw(parse);
 
 =encoding utf8
 
@@ -20,5 +24,26 @@ our $VERSION = '1.00';
 =head1 SYNOPSIS
 
 =cut
+
+sub parse {
+    my @songs;
+    while (my $line = <>) {
+        chomp($line);
+        my @data = split '/', $line;
+        @data = ($data[1], split(' - ', $data[2]), split('\.', $data[3]));
+        my %song = (
+            band   => $data[0],
+            year   => $data[1],
+            album  => $data[2],
+            track  => $data[3],
+            format => $data[4],
+        );
+        push @songs, \%song;
+    }
+    print Dumper \@songs;
+    return \@songs;
+}
+
+
 
 1;
