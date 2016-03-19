df3d5478c06871e6f69c069613bc43e18c27fb75
Evgeniy Zemtsov <zemtsov.e.e@gmail.com>
2016-03-20 02:20:28 +0300

    Add parse stage. Add description

--- homeworks/json-parser/lib/Local/JSONParser.pm
+++ homeworks/json-parser/lib/Local/JSONParser.pm
@@ -4,8 +4,33 @@ use strict;
 use warnings;
 use base qw(Exporter);
 use Local::JSONParser::Lexer qw(tokenize);
+use Local::JSONParser::Parser qw(parse);
 our @EXPORT_OK = qw( parse_json );
 our @EXPORT = qw( parse_json );
+
+
+=encoding utf8
+
+=head1 NAME
+
+Local::JSONParser - JSON Parser
+
+=head1 VERSION
+
+Version 1.00
+
+=cut
+
+our $VERSION = '1.00';
+
+=head1 SYNOPSIS
+
+    package Local::SomePackage;
+    use Local::JSONParser 'parse_json';
+
+    my $data = parse_json( $source );
+
+=cut
 
 sub parse_json {
 	my $source = shift;
@@ -14,8 +39,9 @@ sub parse_json {
 	# return JSON::XS->new->utf8->decode($source);
 
     my $tokens = tokenize($source);
+    my $result = parse($tokens);
 
-	return $tokens;
+	return $result; 
 }
 
 1;
