91add0cc5ddf0af2d9da3a167e4b526aa789b910
Evgeniy Zemtsov <zemtsov.e.e@gmail.com>
2016-03-20 02:16:32 +0300

    Add parser

---
+++ homeworks/json-parser/lib/Local/JSONParser/Parser.pm
@@ -0,0 +1,89 @@
+package Local::JSONParser::Parser;
+
+use strict;
+use warnings;
+use 5.010;
+no warnings 'experimental';
+use base qw(Exporter);
+our @EXPORT_OK = qw(parse);
+use Local::JSONParser::Lexer qw(token_type token_data);
+
+my $tokens;
+
+sub pop_token {
+    my $token = shift(@$tokens) // die('Not enough tokens');
+    my $check = shift // token_type($token);
+
+    die("$check token expected") if $check ne token_type($token);
+
+    return $token;
+}
+
+sub top_token {
+    my $token = $tokens->[0] // die('Not enough tokens');
+
+    return token_type($token);
+}
+
+sub object {
+    pop_token('BRACE_LEFT');
+    my %object;
+    if (top_token() ne 'BRACE_RIGHT') {
+        while (1) {
+            my $key = pop_token('STRING');
+            pop_token('COLON');
+            my $value = value();
+            $object{token_data($key)} = $value;
+        } continue {
+            last if top_token() eq 'BRACE_RIGHT';
+            pop_token('COMMA');
+        }
+    }
+    pop_token('BRACE_RIGHT');
+    
+    return \%object;
+}
+
+sub array { 
+    pop_token('BRACKET_LEFT');
+    my @array;
+    if (top_token() ne 'BRACKET_RIGHT') {
+        while (1) {
+            my $value = value();
+            push @array, $value;
+        } continue {
+            last if top_token() eq 'BRACKET_RIGHT';
+            pop_token('COMMA');
+        }
+    }
+    pop_token('BRACKET_RIGHT');
+
+    return \@array;
+}
+
+sub value {
+    given (top_token()) {
+        when ('BRACE_LEFT') {
+            return object(); 
+        }
+        when ('BRACKET_LEFT') {
+            return array(); 
+        }
+        when (['STRING', 'NUMBER']) {
+            return token_data(pop_token());
+        }
+        default {
+            die('Wrong value');
+        }
+    }
+}
+
+sub parse {
+    $tokens = shift;
+
+    my $value = value();
+
+    return $value;
+}
+
+1;
