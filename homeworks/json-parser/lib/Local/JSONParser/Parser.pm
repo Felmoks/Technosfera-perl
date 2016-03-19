a57ec718891af12f39683bfbce29cbe241913f69
Evgeniy Zemtsov <zemtsov.e.e@gmail.com>
2016-03-20 02:52:48 +0300

    Change top_token interface

--- homeworks/json-parser/lib/Local/JSONParser/Parser.pm
+++ homeworks/json-parser/lib/Local/JSONParser/Parser.pm
@@ -12,7 +12,7 @@ my $tokens;
 
 sub pop_token {
     my $token = shift(@$tokens) // die('Not enough tokens');
-    my $check = shift // token_type($token);
+    my $check = shift           // return $token;
 
     die("$check token expected") if $check ne token_type($token);
 
@@ -21,21 +21,22 @@ sub pop_token {
 
 sub top_token {
     my $token = $tokens->[0] // die('Not enough tokens');
+    my $check = shift        // return token_type($token);
 
-    return token_type($token);
+    return $check eq token_type($token);
 }
 
 sub object {
     pop_token('BRACE_LEFT');
     my %object;
-    if (top_token() ne 'BRACE_RIGHT') {
+    if (!top_token('BRACE_RIGHT')) {
         while (1) {
             my $key = pop_token('STRING');
             pop_token('COLON');
             my $value = value();
             $object{token_data($key)} = $value;
         } continue {
-            last if top_token() eq 'BRACE_RIGHT';
+            last if top_token('BRACE_RIGHT');
             pop_token('COMMA');
         }
     }
@@ -47,12 +48,12 @@ sub object {
 sub array { 
     pop_token('BRACKET_LEFT');
     my @array;
-    if (top_token() ne 'BRACKET_RIGHT') {
+    if (!top_token('BRACKET_RIGHT')) {
         while (1) {
             my $value = value();
             push @array, $value;
         } continue {
-            last if top_token() eq 'BRACKET_RIGHT';
+            last if top_token('BRACKET_RIGHT');
             pop_token('COMMA');
         }
     }
