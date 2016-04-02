49ff93ff4344d3aaada40e4f75465d93a01cd6aa
Evgeniy Zemtsov <zemtsov.e.e@gmail.com>
2016-04-02 15:48:13 +0300

    Add basic array iterator

--- homeworks/iter/lib/Local/Iterator/Array.pm
+++ homeworks/iter/lib/Local/Iterator/Array.pm
@@ -2,6 +2,7 @@ package Local::Iterator::Array;
 
 use strict;
 use warnings;
+use parent 'Local::Iterator';
 
 =encoding utf8
 
@@ -14,5 +15,19 @@ Local::Iterator::Array - array-based iterator
     my $iterator = Local::Iterator::Array->new(array => [1, 2, 3]);
 
 =cut
+
+sub init {
+    my ($self, %args) = @_;
+
+    $self->{data} = [ @{ $args{array} } ];
+}
+
+sub next {
+    my ($self) = @_; 
+
+    my $end = (@{ $self->{data} } == 0);
+    my $val = shift @{ $self->{data} };
+    return ($val, $end);
+}
 
 1;
